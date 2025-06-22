f852689c4eac9a9e88973ea3b76be6bb204d5a10
MokshAditya424 <smokshaditya@gmail.com>
2025-06-22 11:26:42 +0530

    Update Logger.java

--- Logger.java
+++ Logger.java
@@ -2,23 +2,23 @@ package SingletonPattern;
 
 public class Logger {
 	
-	    // Step 1: Create a private static instance of Logger
+	    
 	    private static Logger instance;
 
-	    // Step 2: Private constructor to prevent instantiation
+	    
 	    private Logger() {
 	        System.out.println("Logger instance created.");
 	    }
 
-	    // Step 3: Public static method to provide access to the instance
+	    /
 	    public static Logger getInstance() {
 	        if (instance == null) {
-	            instance = new Logger(); // Lazy initialization
+	            instance = new Logger(); 
 	        }
 	        return instance;
 	    }
 
-	    // Example method
+	
 	    public void log(String message) {
 	        System.out.println("Log: " + message);
 	  }
